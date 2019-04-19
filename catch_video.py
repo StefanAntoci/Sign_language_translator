c6b642017e1d6a70d9c0413d66a91ef4a677ef93
Stefan <stefan.antoci96@gmail.com>
2019-04-20 01:18:20 +0300

    Functions for predicing every 25 frames was added

--- catch_video.py
+++ catch_video.py
@@ -57,7 +57,7 @@ def hist_masking(frame, hist):
     dst = cv2.calcBackProject([hsv], [0, 1], hist, [0, 180, 0, 256], 1)
     disc = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (31, 31))
     cv2.filter2D(dst, -1, disc, dst)    
-    ret, thresh = cv2.threshold(dst, 50, 255, cv2.THRESH_BINARY)
+    ret, thresh = cv2.threshold(dst, 100, 255, cv2.THRESH_BINARY)
     thresh = cv2.merge((thresh, thresh, thresh))
     #return cv2.bitwise_and(thresh, frame)
     return dst, cv2.bitwise_and(thresh, frame)
@@ -75,39 +75,70 @@ def predict(graph, sess, key_points):
     result = sess.run(predOuts, feed_dict)   
     return result
 
+def get_most_popular_letter(letters):
+    letters_app = dict()
+    letters_app[letters[0]] = 1
+    for i in range(1,len(letters)):
+        letter_found = False
+        for key, value in letters_app.items():
+            if letters[i] == key:
+                letters_app[key] = letters_app[key] + 1
+                letter_found = True
+                break
+        if letter_found == False:
+            letters_app[letters[i]] = 1
+            
+    letter = list(letters_app.keys())[0]
+    appearances = list(letters_app.values())[0]
+    for key, value in letters_app.items():
+        if value > appearances:
+            letter = key
+        
+    return letter
     
 def capture_video():
 
     with tf.Session() as sess:
         cap = cv2.VideoCapture(0)    
         x = 0
+        y = 0
         graph = load_model(sess)
+        letters = list()
+        frames = list()
         while(True):
-            x = x + 1
-            ret, frame = cap.read()
-            
             frame = draw_rect(frame)
             hist = hand_histogram(frame)
             prob_img, img = hist_masking(frame, hist)
-            
-            #roi = he.hsv_mask(frame)
-            
-            #symbol, cnt= he.hand_extraction(roi)
-            symbol, cnt = he.hand_extraction(prob_img)
-            cv2.imshow("symbol", symbol)
             cv2.imshow("img", img)
-            #print(cnt)
-            if len(cnt) >= 20:
+            #roi = he.hsv_mask(frame)                
+            #symbol, cnt= he.hand_extraction(roi)
+            contour  = he.get_hand_contour(prob_img)
+            if y < 5:
+                frames.append(contour)
+                y = y + 1
+            else:
+                filtered_contour = he.delete_noize(frames)
+                symbol = he.adjust_hand_contour(filtered_contour)
+                frames.clear()
+                y = 0
+                
                 key_points = he.features_calculation(symbol)
-                key_points = np.asarray(key_points)
-                key_points = key_points.reshape(1,20)
-                #print(key_points)            
-                predictions = predict(graph, sess, key_points)
-                predictions_dev = np.std(predictions)
-                #print(predictions)
-                #if predictions_dev > 15:
-                letter = nn.prob_to_letter(predictions[0])
-                print(letter)            
+                if len(key_points) > 1:
+                    key_points = np.asarray(key_points)
+                    key_points = key_points.reshape(1,20)           
+                    predictions = predict(graph, sess, key_points)
+                    predictions_dev = np.std(predictions)
+                    #print(predictions)
+                    #if predictions_dev > 15:
+                    letter = nn.prob_to_letter(predictions[0])
+                    if x < 5:
+                        letters.append(letter)
+                        x = x + 1
+                    else:
+                        letter = get_most_popular_letter(letters)
+                        letters.clear()
+                        x = 0
+                        print(letter)
             cv2.imshow("Capturing", frame)
             key = cv2.waitKey(1)
             if key == ord('q'):
